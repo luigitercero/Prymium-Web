3208445557a223d5caa6a7ab1723c88f636ba434
luigitercerobold <estuardo@bold.gt>
2020-12-21 17:23:06 -0600

    configuracion de react js para trabajar

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+   entry: './src/index.js',
+   output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+   },
+   resolve: {
+      extensions: ['.js', '.jsx']
+   },
+   module: {
+      rules: [
+         {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+               loader: 'babel-loader'
+            }
+         },
+         {
+            test: /\.html$/,
+            use: [
+               {
+                  loader: 'html-loader'
+               }
+            ]
+         },
+         {
+            test: /\.css$/,
+            use: [
+               {
+                  loader: MiniCSSExtractPlugin.loader,
+               },
+               'css-loader',]
+         },
+         {
+            test: /\.s[ac]ss$/i,
+            use: [
+               // fallback to style-loader in development
+               process.env.NODE_ENV !== "production"
+                  ? "style-loader"
+                  : MiniCssExtractPlugin.loader,
+               "css-loader",
+               "sass-loader",
+            ]
+         }
+      ]
+   },
+   plugins: [
+      new HtmlWebpackPlugin({
+         template: './public/index.html',
+         filename: 'index.html'
+      }),
+      new MiniCSSExtractPlugin({
+         filename: 'assets/[name].css',
+         chunkFilename: 'assets/[id].css',
+
+      })
+   ],
+   devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 3005
+   }
+};
