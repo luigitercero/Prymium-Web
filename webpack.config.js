a67f7dd007c8bd8cb5a3a46f1616c4f10e52f422
luigitercerobold <estuardo@bold.gt>
2020-12-21 18:05:54 -0600

    ruta home inicializada

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
    entry: './src/index.js',
    output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js'
+      filename: 'bundle.js',
+      publicPath:'/'
    },
    resolve: {
       extensions: ['.js', '.jsx']
@@ -64,6 +65,7 @@ module.exports = {
    devServer: {
       contentBase: path.join(__dirname, 'dist'),
       compress: true,
+      historyApiFallback:true,
       port: 3005
    }
 };
