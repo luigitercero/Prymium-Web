f178c80523b8dba2c3b3c8d911a23d553c3fce62
luigitercerobold <luigitercero@gmail.com>
2020-12-23 12:37:49 -0600

    start navigator

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 
 const Header = () => {
-	const hola = () => {
-		const amigos = 25;
-		const bueno = `hola${amigos}`;
-		return bueno;
-	}
-	return (
-  <div>
-    {hola()}
-  </div>
-	)
+  const hola = () => {
+    const amigos = 25;
+    const bueno = `hola${amigos}`;
+    return bueno;
+  }
+  return (
+    <div>
+      <ol start="5">
+        <li>Julio</li>
+        <li>Carmen</li>
+        <li>Ignacio</li>
+        <li>Elena</li>
+      </ol>
+    </div>
+  )
 }
 
 export default Header
