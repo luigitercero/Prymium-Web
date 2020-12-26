a0584313506936ec0ea5eed1a6eb894b2ebe01f0
luigitercerobold <luigitercero@gmail.com>
2020-12-26 13:24:26 -0600

    responsive initial footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,29 @@
 /* eslint-disable arrow-body-style */
-import React from 'react'
+import React from 'react';
+import '../styles/components/footer.scss';
 
 const Footer = () => {
   return (
-    <div>
-      <p>hola</p>
-    </div>
+    <footer className="container-footer">
+      <section>
+
+        <p>Dirección:</p>
+        <p>3a. calle 3-54 boulevard San Cristobal, zona 8 de Mixco, Guatemala, Guatemala</p>
+
+      </section>
+      <section>
+
+        <p>Tel: 2485-5176</p>
+        <p>Cel: 5510-3370</p>
+
+      </section>
+      <section>
+
+        <p>Redes Sociales</p>
+      
+
+      </section>
+    </footer>
   );
 }
 
