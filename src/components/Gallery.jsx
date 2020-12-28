fbf28a3fa8df51b397be820f51e7433d4f80c3a5
Juan Fernando <jfer_32@hotmail.com>
2020-12-28 12:43:31 -0600

    agregando scroll para dispositivos moviles

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,5 @@
-import React, { Fragment } from 'react';
+import React, { useEffect } from 'react';
+import '../styles/components/gallery.scss';
 
 const GalleryItem = ({ element }) => {
   const { image, alt, name, description } = element
@@ -16,32 +17,79 @@ const GalleryItem = ({ element }) => {
 const Gallery = ({ id, title, imageArray }) => {
 
   const animationTime = 300;
+  const maxPosition = imageArray.length;
+  const offset =  parseInt(window.innerWidth / 2);
+  const error = 20;
   let position = 0;
+  let initialScrollPosition = 0;
+  let currentScrollPosition = 0;
+  let acumMovement = 0;
+  let stepSize = 310;
+  let scrollRight = false;
 
+  useEffect(() => {
+    const galleryContainer = document.getElementById(`${id}-container`);
+    galleryContainer.scrollLeft = offset;
+  })
+
+  function touchOut(){
+    const gallery = document.getElementById(id);
+    const galleryContainer = document.getElementById(`${id}-container`);
+    console.log(currentScrollPosition ,currentScrollPosition - offset, parseInt((currentScrollPosition - offset)/stepSize))
+    console.log(offset);
+    const movement = currentScrollPosition - offset
+    const stepsToMove = parseInt(movement / stepSize) 
+    if(movement >= error){
+      acumMovement +=movement;
+      position += stepsToMove;
+      if(scrollRight){
+        position++;
+      }
+    }else if(movement <= -error){
+      acumMovement +=movement;
+      position += stepsToMove;
+      if(!scrollRight){
+        position--;
+      }
+    }
+    console.log(position, stepsToMove, scrollRight, !scrollRight);
+    gallery.style.transform = `translateX(${offset - acumMovement}px)`;
+    galleryContainer.scrollLeft = offset;
+    acumMovement = position*stepSize;
+    gallery.animate({ "transform": `translateX(${-position*stepSize + offset}px)` }, animationTime)
+    setTimeout(() => { gallery.style.transform = `translateX(${-position*stepSize + offset}px)`; }, animationTime)
+  }
 
   function scrollControl() {
-    const gallery = document.getElementById(id);
-    gallery.scrollLeft;
+    const galleryContainer = document.getElementById(`${id}-container`);
+    currentScrollPosition = galleryContainer.scrollLeft;
+    if(currentScrollPosition > initialScrollPosition){
+      scrollRight = true;
+    }else if(currentScrollPosition < initialScrollPosition){
+      scrollRight = false;
+    }
+
+    initialScrollPosition = currentScrollPosition;
   }
 
   function moveRight() {
     const gallery = document.getElementById(id);
-    position += 310;
-    gallery.animate({ "transform": `translateX(-${position}px)` }, animationTime)
-    setTimeout(() => { gallery.style.transform = `translateX(-${position}px)`; }, animationTime)
+    position += 1;
+    gallery.animate({ "transform": `translateX(${-position*stepSize + offset}px)` }, animationTime)
+    setTimeout(() => { gallery.style.transform = `translateX(${-position*stepSize + offset}px)`; }, animationTime)
   }
 
   function moveLeft() {
     const gallery = document.getElementById(id);
-    position -= 310;
-    gallery.animate({ "transform": `translateX(-${position}px)` }, animationTime)
-    setTimeout(() => { gallery.style.transform = `translateX(-${position}px)`; }, animationTime)
+    position -= 1;
+    gallery.animate({ "transform": `translateX(${-position*stepSize + offset}px)` }, animationTime)
+    setTimeout(() => { gallery.style.transform = `translateX(${-position*stepSize + offset}px)`; }, animationTime)
   }
 
   return (
-    <>
+    <div className="image-gallery">
       <h2>{title}</h2>
-      <div className="gallery-container">
+      <div className="gallery-container" id={`${id}-container`} onScroll={scrollControl} onTouchEnd={touchOut}>
         <div className="gallery" id={id}>
           {imageArray.map(element => {
             return (
@@ -51,10 +99,10 @@ const Gallery = ({ id, title, imageArray }) => {
             )
           })}
         </div>
-        <span aria-hidden="true" className="fas fa-angle-right right-arrow" onClick={moveRight} />
-        <span aria-hidden="true" className="fas fa-angle-left left-arrow" onClick={moveLeft} />
       </div>
-    </>
+      <span aria-hidden="true" className="fas fa-angle-right arrow right-arrow" id={id+'-ra'} onClick={moveRight} />
+      <span aria-hidden="true" className="fas fa-angle-left arrow left-arrow" id={id+'-la'} onClick={moveLeft} />
+    </div>
   );
 };
 
