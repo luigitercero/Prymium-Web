82d16f24d485d50eaec61acb2c74a5db78284af4
Fernando Montufar <201403684@ingenieria.usac.edu.gt>
2021-01-13 18:12:05 -0600

    Primera estructura de pagina del blog

---
+++ pages/blog/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Blog from '../../src/containers/Blog/App';
+
+const Principal = () => {
+  return(
+    <Blog />
+  )
+}
+
+export default Principal;
---
+++ src/containers/Blog/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Title } from '../../components/Title';
+
+const Blog = () => {
+  return(
+    <div className="blog-container">
+      <Title>Hello World</Title>
+    </div>
+  )
+}
+
+export default Blog;
