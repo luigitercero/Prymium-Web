aabbfea17278da14bc3f8b6eb5bde05c571303f0
Fernando Montufar <201403684@ingenieria.usac.edu.gt>
2021-01-17 15:51:50 -0600

    Agregando componente de item

--- src/components/BaseAPIPage/App.jsx
+++ src/components/BaseAPIPage/App.jsx
@@ -6,18 +6,33 @@ import { Title, SubTitle2, Pre } from "../Title";
 
 import styles from './styles.module.scss';
 
+const Item = ({ item }) => {
+  const { titulo, description } = item;
+
+  return(
+    <div className={styles.item}>
+      <div>
+        <SubTitle2 className={styles.item_title}>{titulo}</SubTitle2>
+      </div>
+      <div className={styles.item_content}>
+        <Pre>
+          {description}
+        </Pre>
+      </div>
+    </div>
+  )
+
+}
+
 const BaseAPIPage = ({ title, subtitle, url }) => {
 
-  const [data, setData] = useState({
-    titulo:"",
-    description:""
-  });
+  const [data, setData] = useState([]);
 
   useEffect(async () =>{
     const response = await fetch(url);
     const info = await response.json();
-    setData(info[0]);
-    console.log(data.titulo);
+    setData(info);
+    console.log(data);
   }, []);
 
   return(
@@ -28,12 +43,9 @@ const BaseAPIPage = ({ title, subtitle, url }) => {
       </SubHero>
       <div className={styles.content}>
         <section>
-          <div className={styles.answer}>
-            <SubTitle2 className={styles.qtitle}>{data.titulo}</SubTitle2>
-            <Pre>
-              {data.description}
-            </Pre>
-          </div>
+          {data.map(item => {
+            return <Item key={item.id} item={item} />
+          })}
         </section>
       </div>
     </div>
