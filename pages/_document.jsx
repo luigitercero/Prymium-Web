677325e57ab220e96fd85fceae0e11b34fac48e9
luigitercerobold <luigitercero@gmail.com>
2021-02-21 10:20:15 -0600

    chanche name manifest

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -10,13 +10,22 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="es">
-        <Head />
+        <Head >
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MRX968C');</script>
+        </Head>
+  
         <body>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MRX968C"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
           <Main />
           <NextScript />
         </body>
        
-        {/* <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/8323542.js" /> */}
+        <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/8323542.js" />
 
       </Html>
     )
