165dfebd940556a47c790766bb9053237aae2192
luigitercerobold <luigitercero@gmail.com>
2021-04-01 21:02:27 -0600

    modificando head

--- src/hooks/useSEO.jsx
+++ src/hooks/useSEO.jsx
@@ -8,6 +8,14 @@ const useSEO = ({ description, keywords, title, children, img, url }) => {
     <>
     
       <Head>
+        <script dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NKSQN8N');`}}
+        />
+
         <meta charSet="UTF-8" />
         <link rel="shortcut icon" href="/images/icons/water-tap.ico" type="image/x-icon" />
         <title>{title}</title>
