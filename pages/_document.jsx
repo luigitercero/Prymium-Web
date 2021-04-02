03fe3eec9975a5b915b09a7cc3144ef799cb24fa
luigitercerobold <luigitercero@gmail.com>
2021-04-01 19:56:22 -0600

    tag manager 4

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -27,14 +27,14 @@ class MyDocument extends Document {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NKSQN8N');`}}
+          })(window,document,'script','dataLayer','GTM-5M779VJ');`}}
           />
 
         </Head>
 
         <body>
           <noscript dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NKSQN8N"
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5M779VJ"
         height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
           }}
           />
