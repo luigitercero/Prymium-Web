770b801b5721a9fbb56cba2600cafbac35924cf3
luigitercerobold <luigitercero@gmail.com>
2021-04-01 19:33:05 -0600

    tag manager install

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,27 +3,41 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 import TagManager from 'react-gtm-module'
 
 const tagManagerArgs = {
-  id: 'GTM-MRX968C'
+  id: 'GTM-NKSQN8N '
 }
 class MyDocument extends Document {
-  
+
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
     return { ...initialProps }
   }
 
-  componentDidMount () {
+  componentDidMount() {
     TagManager.initialize(tagManagerArgs)
   }
 
   render() {
-   
+
     return (
       <Html lang="es">
-        <Head />
+
+        <Head>
+          <script dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NKSQN8N');`}}
+          />
+
+        </Head>
 
         <body>
-         
+          <noscript dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NKSQN8N"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+          />
           <Main />
           <NextScript />
         </body>
