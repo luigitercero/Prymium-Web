c6512639d6bf5598a38f9833f405103b8f6cb3d4
Chato <luigitercero3@gmail.com>
2021-11-10 10:13:51 -0600

    change data enriquesida

--- src/hooks/useEriquesido.js
+++ src/hooks/useEriquesido.js
@@ -2,57 +2,58 @@
 
 const DatoEriquesido = ({singleProduct,url}) => {
   
-              return `
-              {
-                "@context": "https://schema.org/",
-                "@type": "Product",
-                "name": "${singleProduct[0]?.title || ""} ",
-                "image": [
-                  "${singleProduct[0]?.imagen|| ""}"
-                 ],
-                "description": "${singleProduct[0]?.content || ""}",
-                "sku": "${singleProduct[0]?.id || ""}",
-                "mpn": "${singleProduct[0]?.id || ""}",
-                "brand": {
-                  "@type": "Brand",
-                  "name": "Prymium"
-                },
-                "review": {
-                  "@type": "Review",
-                  "reviewRating": {
-                    "@type": "Rating",
-                    "ratingValue": "0",
-                    "bestRating": "0",
-                    "worstRating": "0"
-                  },
-                  "author": {
-                    "@type": "Person",
-                    "name": "Luis"
-                  }
-                },
-                "offers": {
-                  "@type": "Offer",
-                  "url": "${url || ""}",
-                  "priceCurrency": "GTQ",
-                  "condition": "new",
-                  "price": "${singleProduct[0]?.price.replace(",","") || ""}",
-                  "priceValidUntil": "2022-01-01",
-                  "itemCondition": "https://schema.org/UsedCondition",
-                  "availability": "https://schema.org/InStock"
-                },
-                "additionalProperty": [{
-                  "@type": "PropertyValue",
-                  "propertyID": "1",
-                  "value": "${singleProduct[0]?.id || ""}"
-                }]
-              }
-            `
-  //           ,
-  //           }}
-  // />
-  
-  
-  
+  return `
+  {
+    "@context": "https://schema.org/",
+    "@type": "Product",
+    "name": "${singleProduct[0]?.title || ""} ",
+    "image": [
+      "${singleProduct[0]?.imagen|| ""}"
+     ],
+    "description": "${singleProduct[0]?.content || ""}",
+    "sku": "${singleProduct[0]?.id || ""}",
+    "mpn": "${singleProduct[0]?.id || ""}",
+    "brand": {
+      "@type": "Brand",
+      "name": "Prymium"
+    },
+    "review": {
+      "@type": "Review",
+      "reviewRating": {
+        "@type": "Rating",
+        "ratingValue": "0",
+        "bestRating": "0",
+        "worstRating": "0"
+      },
+      "author": {
+        "@type": "Person",
+        "name": "Luis"
+      }
+    },
+    "offers": {
+      "@type": "Offer",
+      "url": "${url || ""}",
+      "priceCurrency": "GTQ",
+      "condition": "new",
+      "price": "${singleProduct[0]?.price.replace(",","") || ""}",
+      "priceValidUntil": "2022-01-01",
+      "itemCondition": "https://schema.org/NewCondition",
+      "availability": "https://schema.org/InStock"
+      "condition": "new",
+    },
+    "additionalProperty": [{
+      "@type": "PropertyValue",
+      "propertyID": "1",
+      "value": "${singleProduct[0]?.id || ""}"
+    }]
+  }
+`
+//           ,
+//           }}
+// />
+
+
+
 }
 
 export default DatoEriquesido
