f83572f8f3f1deb4990a19f8089ac8f7f771fdbe
Chato <luigitercero3@gmail.com>
2022-01-13 14:45:59 -0600

    redirecciones agregadas

--- next.config.js
+++ next.config.js
@@ -1,65 +1,62 @@
-/* eslint-disable import/newline-after-import */
-	// eslint-disable-next-line import/no-extraneous-dependencies
-  const path = require('path')
-  const withWorkbox = require("next-with-workbox");
-  module.exports = withWorkbox( {
-    sassOptions: {
-      includePaths: [path.join(__dirname, 'src', 'styles')],
-    }, 
-    workbox: {
-      dest: "public",
-      swDest: "sw.js",
-      swSrc: "worker.js",
-      force: true,
-    },
-    async redirects() {
-      return [
-        {
-          source: '/productos-2/',
-          destination: '/tienda',
-          permanent: true,
-        },
-        {
-          source: '/producto-2/',
-          destination: '/tienda',
-          permanent: true,
-        },
-        {
-          source: '/product-2/',
-          destination: '/tienda',
-          permanent: true,
-        },
-        {
-          source: '/store-lavatrastos-grifos-bidet-guatemala/',
-          destination: '/tienda',
-          permanent: true,
-        },
-        {
-          source: '/mantenimiento/',
-          destination: '/preguntas',
-          permanent: true,
-        },
-        {
-          source: '/especificaciones/',
-          destination: '/preguntas',
-          permanent: true,
-        },
-        {
-          source: '/nosotros/',
-          destination: '/preguntas/',
-          permanent: true,
-        },
-        {
-          source: '/garantia/',
-          destination: '/preguntas/',
-          permanent: true,
-        },
-        {
-          source: '/producto/:slug*',
-          destination: '/tienda/:slug*',
-          permanent: true,
-        },
-      ]
-    },
-  })
-  
+const path = require('path')
+const withWorkbox = require("next-with-workbox");
+
+module.exports = withWorkbox({
+  // Uncomment the line below to enable basePath, pages and
+  // redirects will then have a path prefix (`/app` in this case)
+  //
+  // basePath: '/app',
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src', 'styles')],
+  }, 
+  workbox: {
+    dest: "public",
+    swDest: "sw.js",
+    swSrc: "worker.js",
+    force: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/productos-2',
+        destination: '/tienda',
+        permanent: true,
+      },
+      {
+        source: '/store-lavatrastos-grifos-bidet-guatemala',
+        destination: '/tienda',
+        permanent: true,
+      },
+      {
+        source: '/mantenimiento',
+        destination: '/preguntas',
+        permanent: true,
+      },
+      {
+        source: '/especificaciones',
+        destination: '/preguntas',
+        permanent: true,
+      },
+      {
+        source: '/nosotros',
+        destination: '/preguntas',
+        permanent: true,
+      },
+      {
+        source: '/garantia',
+        destination: '/preguntas',
+        permanent: true,
+      },
+      {
+        source: '/producto/:slug*',
+        destination: '/tienda/:slug*',
+        permanent: true,
+      },
+      {
+        source: '/prensa-libre',
+        destination: '/',
+        permanent: false,
+      },
+    ]
+  },
+})
