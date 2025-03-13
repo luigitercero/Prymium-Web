b5d4b777436b36b1ea6d7bad63cf3f9742362488
Luis Azurdia <luigitercero3@gmail.com>
2025-03-13 13:39:11 -0600

    update hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,162 +1,180 @@
-import React, { useState, useRef, useEffect } from 'react'
-import Link from 'next/link';
-import { CSSTransition } from 'react-transition-group';
-import PropTypes from 'prop-types';
-import Image from 'next/image';
-import _route from '@routes/Config';
+import React, { useState, useRef, useEffect } from "react";
+import Link from "next/link";
+import PropTypes from "prop-types";
+import Image from "next/image";
+import { motion, AnimatePresence } from "framer-motion";
+import _route from "@routes/Config";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
   const [keyOpen, setkeyOpen] = useState("#closeMenu");
+
   const onClick = () => {
     setOpen(!open);
-    setkeyOpen(keyOpen === "#openMenu" ? '#closeMenu' : '#openMenu');
-  }
+    setkeyOpen(keyOpen === "#openMenu" ? "#closeMenu" : "#openMenu");
+  };
 
   return (
+    <div className="header">
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            className="overlay"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+            onClick={onClick}
+          />
+        )}
+      </AnimatePresence>
 
-    <div className='header'>
-
-      <CSSTransition
-        in={open}
-        timeout={500}
-        classNames="fadeover"
-        unmountOnExit
-      >
-        <div className="overlay" onClick={onClick} aria-hidden="true" />
-      </CSSTransition>
-
-      <nav className="direction ">
+      <nav className="direction">
         <div className="image-container">
-          <Image src="/images/logo/logotipo-Prymium.webp" fill="true" sizes="(min-width: 600px) 600px, 100vw" alt='Prymium'/>
+          <Image
+            src="/images/logo/logotipo-Prymium.webp"
+            fill="true"
+            sizes="(min-width: 600px) 600px, 100vw"
+            alt="Prymium"
+          />
         </div>
         <div className="menu-container">
-          <HambuergerMenu open={open} keyOpen={keyOpen} onClick={onClick}>
+          <HamburgerMenu open={open} keyOpen={keyOpen} onClick={onClick}>
             <NavigationList onClick={onClick} />
-          </HambuergerMenu>
-          <div className='hide'>
+          </HamburgerMenu>
+          <div className="hide">
             <NavigationList />
           </div>
         </div>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-const NavigationList = ({onClick}) => (
+const NavigationList = ({ onClick }) => (
   <ul>
     <NavItem onClick={onClick} {..._route.home} />
     <NavItem onClick={onClick} {..._route.products} />
     <NavItem onClick={onClick} {..._route.asks} />
-    {/* <NavItem {..._route.blog} /> */}
     <NavItem onClick={onClick} {..._route.contact} />
   </ul>
-  )
+);
 
 const NavItem = ({ id, to, name, children, onClick }) => {
   const [showSubMenu, setSubMenu] = useState(false);
+
   const onPress = () => {
     setSubMenu(!showSubMenu);
-    //onClick(); 
-  }
+  };
 
   const close = () => {
     setSubMenu(false);
-    //onClick(); //close menut principal
-  
-  }
+  };
 
-  const childrenWithProps = React.Children.map(children, child => {
-    // checking isValidElement is the safe way and avoids a typescript error too
-    if (React.isValidElement(child)) {
-      return React.cloneElement(child, { close });
-    }
-    return child;
-  });
-
-  const menu = () => (
-    <>
-      <a href={to} onClick={onPress}>{name}</a>
-      {showSubMenu && childrenWithProps}
-    </>
-    )
+  const childrenWithProps = React.Children.map(children, (child) =>
+    React.isValidElement(child) ? React.cloneElement(child, { close }) : child
+  );
 
   return (
     <li key={id} onClick={close}>
-      {
-        children ? (
-          <OutsideAlerter onClick={close}>
-            {menu()}
-          </OutsideAlerter>
-        )
-          : <Link onClick={close} href={to}>{name}</Link>
-      }
+      {children ? (
+        <OutsideAlerter onClick={close}>
+          <a href={to} onClick={onPress}>
+            {name}
+          </a>
+          <AnimatePresence>
+            {showSubMenu && (
+              <motion.div
+                initial={{ opacity: 0, y: -10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -10 }}
+                transition={{ duration: 0.3 }}
+              >
+                {childrenWithProps}
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </OutsideAlerter>
+      ) : (
+        <Link onClick={close} href={to}>
+          {name}
+        </Link>
+      )}
     </li>
   );
-}
+};
 
 const SubMenu = ({ close }) => (
   <ul className="sub-menu">
-    <li id="lavatrastos" onClick={close} aria-hidden><Link href={`${_route.products.to}lavatrastos`}>Lavatrastos</Link></li>
-    <li id="grifo" onClick={close} aria-hidden><Link href={`${_route.products.to}grifos`}>Grifos</Link></li>
-    <li id="bidet" onClick={close} aria-hidden><Link href={`${_route.products.to}bidets`} onClick={close}>Bidet</Link></li>
-    <li id="all" onClick={close} aria-hidden><Link href={`${_route.products.to}`}>Todos</Link></li>
+    <li id="lavatrastos" onClick={close}>
+      <Link href={`${_route.products.to}lavatrastos`}>Lavatrastos</Link>
+    </li>
+    <li id="grifo" onClick={close}>
+      <Link href={`${_route.products.to}grifos`}>Grifos</Link>
+    </li>
+    <li id="bidet" onClick={close}>
+      <Link href={`${_route.products.to}bidets`} onClick={close}>
+        Bidet
+      </Link>
+    </li>
+    <li id="all" onClick={close}>
+      <Link href={`${_route.products.to}`}>Todos</Link>
+    </li>
   </ul>
-  )
+);
 
-const HambuergerMenu = ({ children, open, onClick, keyOpen }) => (
+const HamburgerMenu = ({ children, open, onClick, keyOpen }) => (
   <div className="container-hamburger">
     <a href={keyOpen} className="hamburger" onClick={onClick}>
-      <span className="icon hamburger-icon"><img src="/images/icons/bars-solid.webp" alt="b" /></span>
+      <span className="icon hamburger-icon">
+        <img src="/images/icons/bars-solid.webp" alt="b" />
+      </span>
     </a>
 
-    <CSSTransition
-      in={open}
-      timeout={1000}
-      classNames="fade"
-      unmountOnExit
-    >
-      {children}
-    </CSSTransition>
-
+    <AnimatePresence>
+      {open && (
+        <motion.div
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: 100 }}
+          transition={{ duration: 0.5 }}
+        >
+          {children}
+        </motion.div>
+      )}
+    </AnimatePresence>
   </div>
-  )
+);
 
 /**
- * Hook that alerts clicks outside of the passed ref
+ * Hook that detects clicks outside of the passed ref
  */
 const UseOutsideAlerter = (ref, onClick) => {
   useEffect(() => {
-    /**
-     * Alert if clicked on outside of element
-     */
     const handleClickOutside = (event) => {
       if (ref.current && !ref.current.contains(event.target)) {
         onClick();
       }
-    }
-    // Bind the event listener
+    };
+
     document.addEventListener("mousedown", handleClickOutside);
-
     return () => {
-      // Unbind the event listener on clean up
-
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [ref]);
-}
+};
 
 /**
- * Component that alerts if you click outside of it
+ * Component that detects outside clicks
  */
 const OutsideAlerter = ({ children, onClick }) => {
   const wrapperRef = useRef(null);
   UseOutsideAlerter(wrapperRef, onClick);
   return <div ref={wrapperRef}>{children}</div>;
-}
-
-OutsideAlerter.propTypes = {
-  children: PropTypes.element.isRequired
 };
 
-export default Header
+OutsideAlerter.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export default Header;
